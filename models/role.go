56242a75d2ac0461186fb7660abdb2e02fd4f53b
Sekhar Chaudhary <75156986+code-sekhar@users.noreply.github.com>
2026-06-15 04:12:47 +0530

    wip:role permission

--- models/role.go
+++ models/role.go
@@ -1,6 +1,7 @@
 package models
 
 type Role struct {
-	ID   uint   `gorm:"primaryKey" `
-	Name string `gorm:"unique;not null"`
+	ID         uint         `gorm:"primaryKey" `
+	Name       string       `gorm:"unique;not null"`
+	Permission []Permission `gorm:"many2many:role_permissions;"`
 }
